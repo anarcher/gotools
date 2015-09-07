2f150b83d24e7fa8c87f7985879bad32fe4ebddc
anarcher <anarcher@gmail.com>
2015-09-07 15:08:17 +0900

    Add Goopfile

---
+++ pkg.go
@@ -0,0 +1,13 @@
+package pkg
+
+import (
+	_ "github.com/fzipp/pythia"
+	_ "github.com/golang/lint/golint"
+	_ "github.com/jstemmer/gotags"
+	_ "github.com/mgumz/vopher"
+	_ "github.com/nsf/gocode"
+	_ "github.com/tools/godep"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/tools/cmd/gorename"
+	_ "golang.org/x/tools/cmd/oracle"
+)
